fb465bf57c241747f8abd0050c949a6f0641d70c
pankajksingh189 <pankajksingh189@gmail.com>
2022-01-22 22:45:59 +0530

    Working code

---
+++ src/main/Demo.java
@@ -0,0 +1,34 @@
+package main;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+	static String appUrl="https://www.google.co.in";
+	static String chromedriverLoc=".\\src\\test\\chromedriver.exe";
+	static String xlsLoc=".\\src\\test\\Datasheet.xls";
+	
+	static XlsHelper xls=new XlsHelper(xlsLoc);
+	
+	public static void main(String[] args) {
+//		System.out.println("Launching Application.");
+//		System.setProperty("webdriver.chrome.driver", chromedriverLoc);
+//		WebDriver driver=new ChromeDriver();
+//		driver.get(appUrl);
+//		System.out.println("Page Title: "+driver.getTitle());
+//		driver.quit();
+		
+		xls.getColValuesByColNum(0);
+		xls.getColValuesByColNum(1);
+		xls.getColValuesByColNum(2);
+		xls.getColValuesByColNum(3);
+		xls.getAllValuesColWise();
+		
+	}
+	
+	
+	
+	
+	
+
+}
